81285b0df44715736fd5674dd4d6ebd73b0cc7f7
Hiep Nguyen Van <hiepnv@hblab.com>
2021-09-22 15:52:08 +0700

    get balance token

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
+import React from "react";
 import { useWeb3React, Web3ReactProvider } from "@web3-react/core";
 import { Web3Provider } from "@ethersproject/providers";
-import React from "react";
 import { formatEther } from "@ethersproject/units";
 import { InjectedConnector } from "@web3-react/injected-connector";
-// import your favorite web3 convenience library here
+import useSWR from "swr";
+import ERC20Token from "./components/ERC20Token";
+
+const fetcher =
+  (library) =>
+  (...args) => {
+    const [method, ...params] = args;
+    console.log({ method, params });
+    return library[method](...params);
+  };
 
 export const injectedConnector = new InjectedConnector({
-  supportedChainIds: [97],
+  supportedChainIds: [1, 97],
 });
 
 function getLibrary(provider, _connector) {
@@ -16,55 +25,24 @@ function getLibrary(provider, _connector) {
 function App() {
   return (
     <Web3ReactProvider getLibrary={getLibrary}>
+      <Wallet />
       <Balance />
+      <ERC20Token />
     </Web3ReactProvider>
   );
 }
 
-function Balance() {
-  const account = "0x9c858484b4d35F0161d55a6D0dcE40204D459ef7";
-  const { chainId, activate, active, library } = useWeb3React();
-  const [balance, setBalance] = React.useState();
-
-  const [accountId, setAccountId] = React.useState(null);
-
-  React.useEffect(() => {
-    if (!!accountId && !!library) {
-      library
-        .getBalance(accountId)
-        .then((balance) => {
-          setBalance(balance);
-        })
-        .catch(() => {
-          setBalance(null);
-        });
-
-      return () => {
-        setBalance(undefined);
-      };
-    }
-  }, [account, library, chainId]);
+function Wallet() {
+  const { chainId, account, activate, active } = useWeb3React();
 
   const onClick = () => {
     activate(injectedConnector);
   };
 
-
   return (
     <div>
-      <input type="text" onChange={(e) => setAccountId(e.target.value)}/>
       <div>ChainId: {chainId}</div>
-      <span>Balance</span>
-      <span role="img" aria-label="gold">
-        💰
-      </span>
-      <span>
-        {balance === null
-          ? "Error"
-          : balance
-          ? `Ξ ${parseFloat(formatEther(balance)).toPrecision(4)}`
-          : ""}
-      </span>
+      <div>Account: {account}</div>
       {active ? (
         <div>✅ </div>
       ) : (
@@ -74,6 +52,19 @@ function Balance() {
       )}
     </div>
   );
+}
+
+function Balance() {
+  const { library, account } = useWeb3React();
+
+  const { data: balance } = useSWR(["getBalance", account, "latest"], {
+    fetcher: fetcher(library),
+  });
+
+  if (!balance) {
+    return <div>...</div>;
+  }
+  return <div>Ξ {parseFloat(formatEther(balance)).toPrecision(4)}</div>;
 }
 
 export default App;
