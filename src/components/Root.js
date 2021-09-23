6fb67a2f7e91788f205a150acc89af56591762c5
Hiep Nguyen Van <hiepnv@hblab.com>
2021-09-23 14:55:01 +0700

    feat: wallet connect qr

--- src/components/Root.js
+++ src/components/Root.js
@@ -4,7 +4,12 @@ import NativeBalance from "../components/NativeBalance";
 import Wallet from "../components/Wallet";
 
 export default function Root() {
-  const { library } = useWeb3React();
+  const { library, deactivate, connector } = useWeb3React();
+
+  const handleDisconnect = () => {
+    deactivate(connector);
+  };
+
   return (
     <div style={{ width: "500px", margin: "30px auto" }}>
       <Wallet />
@@ -15,6 +20,9 @@ export default function Root() {
           <hr />
           <ERC20Token />
           <hr />
+          <button type="button" onClick={handleDisconnect}>
+            Disconnect
+          </button>
         </>
       )}
     </div>
--- src/components/Wallet.js
+++ src/components/Wallet.js
@@ -1,30 +1,49 @@
 import { useWeb3React } from "@web3-react/core";
 import { InjectedConnector } from "@web3-react/injected-connector";
+import { WalletConnectConnector } from "@web3-react/walletconnect-connector";
 
-export const injectedConnector = new InjectedConnector({
+const injectedConnector = new InjectedConnector({
   supportedChainIds: [1, 97],
+});
+
+const walletConnector = new WalletConnectConnector({
+  rpc: { 97: "https://data-seed-prebsc-1-s1.binance.org:8545/" },
+  qrcode: true,
 });
 
 export default function Wallet() {
   const { chainId, account, activate, active } = useWeb3React();
 
-  const handleConnectWallet = () => {
+  const handleWalletInjectorActivate = () => {
     activate(injectedConnector);
+  };
+
+  const handleWalletConnect = () => {
+    activate(walletConnector);
   };
 
   return (
     <div>
       {active ? (
         <>
-          <div>✅ <b>Connected</b></div>
+          <div>
+            ✅ <b>Connected</b>
+          </div>
           <div>ChainId: {chainId}</div>
           <div>Account: {account}</div>
         </>
       ) : (
         <>
           <div>You are not connect to any Wallet</div>
-          <button type="button" onClick={handleConnectWallet}>
+          <button type="button" onClick={handleWalletInjectorActivate}>
             Connect Wallet
+          </button>
+          <button
+            type="button"
+            onClick={handleWalletConnect}
+            style={{ marginLeft: "8px" }}
+          >
+            Wallet Connect QR
           </button>
         </>
       )}
